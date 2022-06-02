b3652e7f1a26348cb5ae06fd934b5c0feed27f94
Christian Gordon <csgord23@uw.edu>
2022-06-01 21:58:38 -0700

    Motor runs now

--- Motor Unit/MotorFirmware/main.h
+++ Motor Unit/MotorFirmware/main.h
@@ -42,8 +42,8 @@
         #define TX_DATA_SIZE            (100u)
 
         //Status and Data Structs
-        //CANPacket can_recieve;
-        //CANPacket can_send;
+        CANPacket can_recieve;
+        CANPacket can_send;
         
         //void check_Pin(void);
             struct Error
